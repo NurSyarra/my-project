f9273a9a558120d287ba7e67dd593de465039ec0
NurSyarra <47972765+NurSyarra@users.noreply.github.com>
2019-02-25 18:28:56 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello syarra</Text>
+        <Text>Hello syarraaa</Text>
       </View>
     );
   }
